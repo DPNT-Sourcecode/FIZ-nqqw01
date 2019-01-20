8888244a5f69490aeddc01c7ab51efca79470599
sbx_user1060 <sbx_user1060@ip-10-128-75-33.eu-west-2.compute.internal>
2019-01-20 19:22:34 +0000

    Sun Jan 20 19:22:34 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -8,15 +8,26 @@ public class FizzBuzzSolution {
     	String retVal = "";
     	boolean isFizz = number % 3 == 0 || value.contains("3");
     	boolean isBuzz = number % 5 == 0 || value.contains("5");
+    	boolean isDeluxe = number > 10 && allDigitsIdentical(value);
 		if(isFizz) {
 			retVal = "fizz";
     	}
 		if(isBuzz) {
-    		if(retVal.length() != 0) {
-    			retVal += " ";
-    		}
+    		retVal = addSpaceToNonEmptyString(retVal);
     		retVal += "buzz";
     	}
-    	return (isFizz || isBuzz) ? retVal : value;
+		if(isDeluxe) {
+    		retVal = addSpaceToNonEmptyString(retVal);
+    		retVal += "deluxe";
+		}
+    	return (isFizz || isBuzz || isDeluxe) ? retVal : value;
     }
+
+	private String addSpaceToNonEmptyString(String retVal) {
+		if(retVal.length() != 0) {
+			retVal += " ";
+		}
+		return retVal;
+	}
 }
+
